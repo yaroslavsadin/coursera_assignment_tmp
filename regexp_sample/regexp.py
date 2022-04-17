c8ace21194944f6c781bcbd84c955ceaeae5a03f
Yaroslav Sadin <yaroslav.sadin@gmail.com>
2022-04-17 21:37:19 +0300

    regexp ready

--- regexp_sample/regexp.py
+++ regexp_sample/regexp.py
@@ -1,7 +1,13 @@
 def calculate(data, findall):
-    matches = findall(r"")  # Если придумать хорошую регулярку, будет просто
+    matches = findall(r"([abc])([+-]?=)([abc]?)([+-]?\d+)?")  # Если придумать хорошую регулярку, будет просто
     for v1, s, v2, n in matches:  # Если кортеж такой структуры: var1, [sign]=, [var2], [[+-]number]
         # Если бы могло быть только =, вообще одной строкой все считалось бы, вот так:
-        data[v1] = data.get(v2, 0) + int(n or 0)
+        rhs = data.get(v2, 0) + int(n or 0)
+        if s == '+=':
+            data[v1] += rhs
+        elif s == '-=':
+            data[v1] -= rhs
+        else:
+            data[v1] = rhs
 
     return data
