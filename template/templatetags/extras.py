7a45d8223e6535fd4bae151a92448bcea29ed797
Yaroslav Sadin <yaroslav.sadin@gmail.com>
2022-07-29 16:40:49 +0000

    extras

--- template/templatetags/extras.py
+++ template/templatetags/extras.py
@@ -1,1 +1,17 @@
 from django import template
+
+
+register = template.Library()
+
+
+@register.filter
+def inc(value, incby):
+    return int(value) + int(incby)
+
+
+@register.simple_tag
+def division(divident, divisor, to_int=False):
+    left = int(divident)
+    right = int(divisor)
+    res = left / right
+    return int(res) if to_int else res
