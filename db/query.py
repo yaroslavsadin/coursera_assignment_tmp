89d3d6063594327023daf3913bd5ea2c7142b2f0
Yaroslav Sadin <yaroslav.sadin@gmail.com>
2022-08-03 16:48:41 +0000

    create delete

--- db/query.py
+++ db/query.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from turtle import title
 
 from django.db.models import Q, Count, Avg
 from pytz import UTC
@@ -7,24 +8,66 @@ from db.models import User, Blog, Topic
 
 
 def create():
-    pass
+    # Создать пользователя first_name = u1, last_name = u1.
+    u1 = User(first_name='u1', last_name='u1')
+    u1.save()
+    # Создать пользователя first_name = u2, last_name = u2.
+    u2 = User(first_name='u2', last_name='u2')
+    u2.save()
+    # Создать пользователя first_name = u3, last_name = u3.
+    u3 = User(first_name='u3', last_name='u3')
+    u3.save()
+    # Создать блог title = blog1, author = u1.
+    blog1 = Blog(title='blog1', author=u1)
+    blog1.save()
+    # Создать блог title = blog2, author = u1.
+    blog2 = Blog(title='blog2', author=u1)
+    blog2.save()
+    # Подписать пользователей u1 u2 на blog1, u2 на blog2.
+    blog1.subscribers.add(u1)
+    blog1.subscribers.add(u2)
+    blog1.save()
+    blog2.subscribers.add(u2)
+    blog2.save()
+    # Создать топик title = topic1, blog = blog1, author = u1.
+    topic1 = Topic(title='topic1', blog=blog1, author=u1)
+    topic1.save()
+    # Создать топик title = topic2_content, blog = blog1, author = u3, created = 2017-01-01.
+    topic2 = Topic(
+        title='topic2_content', blog=blog1,
+        author=u3, created=datetime(2017, 1, 1)
+    )
+    topic2.save()
+    # Лайкнуть topic1 пользователями u1, u2, u3.
+    topic1.likes.add(u1)
+    topic1.likes.add(u2)
+    topic1.likes.add(u3)
+    topic1.save()
 
 
 def edit_all():
-    pass
+    users = User.objects.all()
+    for user in users:
+        user.first_name = 'uu1'
+        user.save()
 
 
 def edit_u1_u2():
-    pass
+    users = User.objects.filter(first_name__in=['u1', 'u2']).all()
+    for user in users:
+        user.first_name = 'uu1'
+        user.save()
 
 
 def delete_u1():
-    pass
+    User.objects.get(first_name='u1').delete()
 
 
 def unsubscribe_u2_from_blogs():
-    pass
-
+    blogs = Blog.objects.all()
+    user = User.objects.get(first_name='u2')
+    for blog in blogs:
+        blog.subscribers.remove(user)
 
 def get_topic_created_grated():
     pass
